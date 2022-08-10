ba073dca1d7314ca8e7d55ff747c5e0d94fac4e7
Nebby <nebby131999@gmail.com>
2022-08-09 21:04:41 -0400

    Added custom hookgen, began testing

---
+++ Editor/MMHookGeneratorProcessor.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using ThunderKit.Core.Config;
+using ThunderKit.Core.Data;
+using UnityEditor;
+using Debug = UnityEngine.Debug;
+using UObject = UnityEngine.Object;
+
+namespace RiskOfThunder.RoR2Importer
+{
+    public class MMHookGeneratorProcessor : AssemblyProcessor
+    {
+        public override int Priority => 500;
+        public override string Name => $"MMHook Generator Processor";
+
+        public override string Process(string assemblyPath)
+        {
+            MMHookGeneratorConfiguration dataStorer = MMHookGeneratorConfiguration.GetDataStorer();
+            //Publicizer not enabled? dont publicize
+            if (!dataStorer.enabled)
+                return assemblyPath;
+
+            var assemblyFileName = Path.GetFileName(assemblyPath);
+            //assembly file name is not in assemblyNames? dont publicize.
+            if (!dataStorer.assemblyNames.Contains(assemblyFileName))
+                return assemblyPath;
+
+            UObject hookGenExe = dataStorer.hookGenExecutable;
+            if (hookGenExe == null)
+            {
+                Debug.LogWarning($"Could not strip assembly {assemblyFileName}, as HookGen has not been located.");
+                return assemblyPath;
+            }
+
+            string ror2ManagedDir = ThunderKitSetting.GetOrCreateSettings<ThunderKitSettings>().ManagedAssembliesPath;
+
+            if (!Directory.Exists(Constants.Paths.HookGenAssembliesFolder))
+            {
+                Directory.CreateDirectory(Constants.Paths.HookGenAssembliesFolder);
+            }
+            string outputPath = Path.Combine(Constants.Paths.HookGenAssembliesFolder, assemblyFileName);
+            string hookGenPath = Path.GetFullPath(AssetDatabase.GetAssetPath(hookGenExe));
+
+            List<string> arguments = new List<string>
+            {
+                assemblyPath
+            };
+
+            List<string> log = new List<string> { $"Publicized {assemblyFileName} with the following arguments:" };
+            log.AddRange(GenerateHooks(arguments, hookGenPath));
+            Debug.Log(string.Join("\n", log));
+
+            return assemblyPath;
+            //return outputPath;
+        }
+
+        private List<string> GenerateHooks(List<string> arguments, string hookGenPath)
+        {
+            var logger = new List<string>();
+            for (int i = 0; i < arguments.Count; i++)
+            {
+                logger.Add($"Argument {i}: {arguments[i]}");
+            }
+
+            ProcessStartInfo psi = new ProcessStartInfo(hookGenPath)
+            {
+                WorkingDirectory = Path.GetDirectoryName(hookGenPath),
+            };
+
+            psi.Arguments = string.Join(" ", arguments.Select(arg => $"\"{arg}\""));
+
+            var process = System.Diagnostics.Process.Start(psi);
+            process.WaitForExit(5000);
+            return logger;
+        }
+    }
+}
